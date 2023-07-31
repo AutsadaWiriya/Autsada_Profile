39876c33213e213001f71df7fce74ec4c82a97a9
Autsada Wiriya <aut0935261784@gmail.com>
2023-07-31 18:25:50 +0700

    fix og

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,19 +1,26 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false;
+import Head from 'next/head'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'AutsadaWiriya'
-}
+  title: "AutsadaWiriya",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta
+          property="og:image"
+          content="https://9kritsada.github.io/Hitman.GG/img/logo/hit-man-ogmeta-img.png"
+        />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <meta property="og:image" content="/img/Profile.jpg" />
+        <meta property="og:image" content="https://9kritsada.github.io/Hitman.GG/img/logo/hit-man-ogmeta-img.png" />
       </Head>
       <main className="bg-[#04051C]">
         <Header />
