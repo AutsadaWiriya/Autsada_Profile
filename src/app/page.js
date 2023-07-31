e70f34530b8a6196c30f560837812e1655c691f8
Autsada Wiriya <aut0935261784@gmail.com>
2023-07-31 18:20:56 +0700

    Add og

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,18 @@
 import React from "react";
 import Header from "@/components/Header";
 import Info from "@/components/Info";
+import Head from 'next/head'
 
 export default function Home() {
   return (
-    <main className="bg-[#04051C]">
-      <Header />
-      <Info />
-    </main>
+    <>
+      <Head>
+        <meta property="og:image" content="/img/Profile.jpg" />
+      </Head>
+      <main className="bg-[#04051C]">
+        <Header />
+        <Info />
+      </main>
+    </>
   );
 }
